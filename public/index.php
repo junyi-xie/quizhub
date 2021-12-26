2549dbb443fc071e21bf2157a4fdec65ac2f99e4
Junyi Xie <yunyi.xie@outlook.com>
2021-12-26 16:00:36 +0100

    tailwindcss

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
     include_once dirname(__DIR__) . '/config/config.php';
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+    </body>
+    </html>
